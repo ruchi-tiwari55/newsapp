1cec5e5d626b0e4be63ca35dea6d109ec771937e
Your Name <ruchit215@gmail.com>
2023-06-01 18:07:32 +0530

    Update all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import React from 'react';
+
+import { useState } from 'react';
+import axios from 'axios'
+
+function BasicExample() 
+  {
+       const [data,setData]=useState([])
+
+         const getNews =()=>{
+           axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=97e2b53e663c49a6b78cf2fc589bf6fa')
+           .then((response)=>{
+             setData(response.data.articles)
+          })
+    
+        }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='container'>
+    <button className='btn btn-primary' onClick={getNews}>International News</button>
+    <div className='container'>
+      <div className='row'>
+        { 
+            data.map((value)=>{
+              return(
+              <div className='col'>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={value.urlToImage} />
+                <Card.Body>
+                  <Card.Title>{value.title}</Card.Title>
+                  <Card.Text>{value.description}</Card.Text>
+                  <Button variant="primary">Main News</Button>
+                </Card.Body>
+              </Card>
+              
+              </div>
+              )
+            })
+      
+  }
+   
+    
     </div>
+    </div>
+    </div>
+    </>
   );
 }
 
-export default App;
+export default BasicExample;
